292904700523762a0887680fd324dea189c42305
kishandonga <kishandonga.92@gmail.com>
2020-07-29 19:01:20 +0530

    - EasyPrefs Release v1.0

--- app/src/main/java/com/sample/easyprefs/MainActivity.kt
+++ app/src/main/java/com/sample/easyprefs/MainActivity.kt
@@ -2,13 +2,10 @@ package com.sample.easyprefs
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import io.easyprefs.Prefs
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Prefs.write().stringSetAsync("", HashSet())
     }
 }
